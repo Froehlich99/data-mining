980fae91f896bbfc83d08a822d6307ba65ac829a
dattito <32016658+dattito@users.noreply.github.com>
2026-04-17 12:34:45 +0900

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "UMA Assignments dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python314;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+            pkgs.uv
+            pkgs.texlive.combined.scheme-full
+
+            # required for xgboost on mac
+            pkgs.llvmPackages.openmp
+          ];
+
+          shellHook = ''
+            export UV_PYTHON=${python}/bin/python3
+            export DYLD_LIBRARY_PATH=${pkgs.llvmPackages.openmp}/lib''${DYLD_LIBRARY_PATH:+:$DYLD_LIBRARY_PATH}
+            cd code
+            if [ ! -d .venv ] || [ pyproject.toml -nt .venv ] || [ uv.lock -nt .venv ]; then
+              touch .venv
+            fi
+            source .venv/bin/activate
+            cd ..
+          '';
+        };
+      }
+    );
+}
